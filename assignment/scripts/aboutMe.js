a64cdf5b6a006ff1b160e5737f6d94a727508750
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-01-26 14:37:58 -0600

    Tidied up

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -78,7 +78,7 @@ if (pets >= friendsPets) {
 }
 else if (friendsPets >= pets){
     var mostPets = friendsPets;
-}
+} // end mostPets
 console.log( mostPets );
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
